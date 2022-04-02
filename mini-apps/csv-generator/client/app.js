e104779c2a3bfe10668f69ae89eb13c38bbf64d2
Jon Personal <jtang312@gmail.com>
2022-04-01 22:52:18 -0400

    Refactored to use AJAX requests with FileReader for reading uploaded file content. Added download button functionality

--- mini-apps/csv-generator/client/app.js
+++ mini-apps/csv-generator/client/app.js
@@ -2,9 +2,10 @@ $(document).ready(() => {
   console.log('loaded');
   $('form').on('submit', (event) => {
     event.preventDefault();
-    var filename = document.getElementById('data').files[0];
-    console.log(filename);
+    var input = document.querySelector(`input[type="file"]`).files[0];
+    console.log(input);
     let reader = new FileReader();
+    // .onload invoked after FileReader is done reading the file. Results in reader.result.
     reader.onload = function(evt) {
       var fileData = reader.result;
       console.log('file data: ', fileData);
@@ -16,11 +17,13 @@ $(document).ready(() => {
         },
         success: (data) => {
           console.log('success');
+          data = `<div class="results">${data}</div>`;
           $('.results').replaceWith(data);
+          $('#download').css('display', 'block');
         }
       })
     };
-    reader.readAsText(filename);
+    reader.readAsText(input);
   })
   
 });
